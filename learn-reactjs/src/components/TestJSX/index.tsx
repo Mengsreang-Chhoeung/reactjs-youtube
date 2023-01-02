12e80b2040db54889f0f996d7bd652229b6c37cd
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2023-01-02 19:13:29 +0700

    add object to test jsx component

--- learn-reactjs/src/components/TestJSX/index.tsx
+++ learn-reactjs/src/components/TestJSX/index.tsx
@@ -1,6 +1,18 @@
 const name = "React";
 const greetingTitle = "JavaScript";
 
+interface MyObject {
+  id: number;
+  name: string;
+  position: string;
+}
+
+const myObject: MyObject = {
+  name: "kok",
+  id: 1,
+  position: "Developer"
+}
+
 function TestJSX() {
   return (
     <>
@@ -8,6 +20,12 @@ function TestJSX() {
         Hello {name} {1 + 2}
       </h1>
       <h1 title={greetingTitle}>Hello JavaScript</h1>
+
+      <ul> Object Person:
+        <li>ID: {myObject["id"]}</li>
+        <li>Name: {myObject.name.toUpperCase()}</li>
+        <li>Position: {myObject.position}</li>
+      </ul>
     </>
   );
 }
