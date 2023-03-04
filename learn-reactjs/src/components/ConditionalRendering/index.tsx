518412f80c372e2f73b46dcfb2c34493e5cf08e0
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2023-03-04 17:49:01 +0700

    add ternary operator to conditional rendering exercise

--- learn-reactjs/src/components/ConditionalRendering/index.tsx
+++ learn-reactjs/src/components/ConditionalRendering/index.tsx
@@ -5,13 +5,33 @@ interface ItemProps {
   isPacked: boolean;
 }
 
+// using if else statement
+// const Item: React.FC<ItemProps> = (props) => {
+//   const { name, isPacked } = props;
+
+//   if (isPacked) {
+//     return <li>{name} ✅</li>;
+//   }
+//   return <li>{name}</li>;
+// };
+
+// using if else statement and if true return null
+// const Item: React.FC<ItemProps> = (props) => {
+//   const { name, isPacked } = props;
+
+//   if (isPacked) {
+//     return null;
+//   }
+//   return <li>{name}</li>;
+// };
+
+// using ternary operator
 const Item: React.FC<ItemProps> = (props) => {
   const { name, isPacked } = props;
 
-  if (isPacked) {
-    return <li>{name} ✅</li>;
-  }
-  return <li>{name}</li>;
+  // return <li>{name} {isPacked ? "✅" : "" }</li>;
+
+  return isPacked ? null : <li>{name}</li>;
 };
 
 export default Item;
