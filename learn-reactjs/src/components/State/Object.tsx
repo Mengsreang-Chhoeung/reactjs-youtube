1b328752d075c8448ad3061545066b887781a236
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2023-05-03 23:16:18 +0700

    teaching about spread object in state

--- learn-reactjs/src/components/State/Object.tsx
+++ learn-reactjs/src/components/State/Object.tsx
@@ -13,15 +13,25 @@ const ObjectState: React.FC = () => {
 
   console.log("Hello Student Data: ", studentData);
 
+  const handleIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value === "" ? "0" : e.target.value;
+    setStudentData({ ...studentData, id: parseInt(value) });
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value === "" ? "n/a" : e.target.value;
+    setStudentData({ ...studentData, name: value });
+  };
+
   return (
     <div>
       <div>
         <label htmlFor="id">ID: </label>
-        <input type="number" />
+        <input type="number" onChange={handleIdChange} />
       </div>
       <div>
         <label htmlFor="name">Name: </label>
-        <input type="text" />
+        <input type="text" onChange={handleNameChange} />
       </div>
 
       <br />
