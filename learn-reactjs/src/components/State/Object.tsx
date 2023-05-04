9841eae13ce94f6b095eb95db7f171474bcb6fde
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2023-05-04 22:32:34 +0700

    teaching about updating nested object in state

--- learn-reactjs/src/components/State/Object.tsx
+++ learn-reactjs/src/components/State/Object.tsx
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
 
+type StudentTypeContact = {
+  phone: string;
+  address: string;
+};
+
 type StudentType = {
   id: number;
   name: string;
+  contact: StudentTypeContact;
 };
 
 const ObjectState: React.FC = () => {
   const [studentData, setStudentData] = useState<StudentType>({
     id: 1,
     name: "kok",
+    contact: {
+      phone: "855-12345678",
+      address: "Phnom Penh",
+    },
   });
 
   console.log("Hello Student Data: ", studentData);
@@ -23,6 +33,22 @@ const ObjectState: React.FC = () => {
     setStudentData({ ...studentData, name: value });
   };
 
+  const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value === "" ? "n/a" : e.target.value;
+    setStudentData({
+      ...studentData,
+      contact: { ...studentData.contact, phone: value },
+    });
+  };
+
+  const handleAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value === "" ? "n/a" : e.target.value;
+    setStudentData({
+      ...studentData,
+      contact: { ...studentData.contact, address: value },
+    });
+  };
+
   return (
     <div>
       <div>
@@ -33,6 +59,14 @@ const ObjectState: React.FC = () => {
         <label htmlFor="name">Name: </label>
         <input type="text" onChange={handleNameChange} />
       </div>
+      <div>
+        <label htmlFor="phone">Phone: </label>
+        <input type="text" onChange={handlePhoneChange} />
+      </div>
+      <div>
+        <label htmlFor="address">Address: </label>
+        <input type="text" onChange={handleAddressChange} />
+      </div>
 
       <br />
 
@@ -40,6 +74,13 @@ const ObjectState: React.FC = () => {
         Result:
         <div>ID: {studentData !== undefined ? studentData.id : "n/a"}</div>
         <div>Name: {studentData !== undefined ? studentData.name : "n/a"}</div>
+        <div>
+          Phone: {studentData !== undefined ? studentData.contact.phone : "n/a"}
+        </div>
+        <div>
+          Address:{" "}
+          {studentData !== undefined ? studentData.contact.address : "n/a"}
+        </div>
       </div>
     </div>
   );
